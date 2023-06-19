7f3ea933bdc935515bc03650444593880be5aeab
Michał <michallzxy@gmail.com>
2023-06-19 19:26:23 +0200

    init

---
+++ SubTube/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SubTube.Models;
+using System.Diagnostics;
+
+namespace SubTube.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SubTube/Models/ChannelViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SubTube.Models
+{
+    public class ChannelViewModel
+    {
+        public string ChannelId { get; set; }
+        public string ChannelName { get; set; } = string.Empty;
+
+    }
+}
